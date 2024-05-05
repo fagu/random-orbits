1aeae770e1d25347bd74bdf1f1ebd8e2f91d1fea
Fabian Gundlach <git@fabiangundlach.org>
2024-05-05 17:11:41 +0200

    Benchmark r = 1

--- benchmark.py
+++ benchmark.py
@@ -4,8 +4,8 @@ import os
 import sys
 import time
 
-ps = [[2**10, int(pow(2, 0.25*k))] for k in range(3*4,10*4)]
-ps += [[2**min(10,23-k), 2**k] for k in range(10,19)]
+ps = [[2**10, int(pow(2, 0.5*k))] for k in range(3*2,19*2)]
+# ps += [[2**min(10,23-k), 2**k] for k in range(10,19)]
 
 for [a, b] in ps:
 	print(b, file=sys.stderr)
@@ -13,6 +13,9 @@ for [a, b] in ps:
 	start = time.time()
 	os.system(f"/usr/bin/python3 -c 'import sys; sys.set_int_max_str_digits(0); print(2**{b})' | ./random-cubic {a} 3 - > /dev/null")
 	end = time.time()
+	start2 = time.time()
+	os.system(f"/usr/bin/python3 -c 'import sys; sys.set_int_max_str_digits(0); print(2**{b})' | ./random-cubic {a} 1 - > /dev/null")
+	end2 = time.time()
 	# print(end - start)
-	print(b, (end - start) / a)
+	print(b, (end - start) / a, (end2 - start2) / a)
 	sys.stdout.flush()
